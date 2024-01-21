31380d79c5b34c3a4b7e99c3f37bdd19210dcec2
Simon <87994372+Zarbose@users.noreply.github.com>
2024-01-21 14:12:29 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
+sudo mkdir /usr/local/sound
+sudo mkdir /usr/local/securite
 
-git clone https://github.com/Zarbose/REE.git
-cd REE
-
-chmod 755 REE-DEB/DEBIAN/postinst
-chmod 755 REE-DEB/DEBIAN/preinst
-chmod 755 REE-DEB/usr/local/bin/hello
-dpkg-deb --build --root-owner-group REE-DEB
-
-sudo dpkg -i REE-DEB.deb
+sudo mv usr/local/* /usr/local/
