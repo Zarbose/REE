ee01a7f6b7b67435e08af68474c72276dd33bcf2
Zarbose <simonpieto.pieto@gmail.com>
2024-02-03 18:30:15 +0100

    auto detect eth0

--- REE-DEB/usr/local/bin/eth0.sh
+++ REE-DEB/usr/local/bin/eth0.sh
@@ -13,8 +13,8 @@ ip=$(ifconfig eth0 | grep 'inet ' | awk  '{print $2}')
 
 if echo $ip | grep -q "192.168.199"; then
     log "ETH0 UP -> bridge"
-    # /usr/local/bin/bridge.sh
+    /usr/local/bin/bridge.sh
 else
     log "ETH0 UP -> Gateway"
-    # /usr/local/bin/gateway.sh
+    /usr/local/bin/gateway.sh
 fi
