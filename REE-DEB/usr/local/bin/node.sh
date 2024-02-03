03603e4bb37610c5f067b152b1582e37810b2de3
Zarbose <simonpieto.pieto@gmail.com>
2024-02-03 18:29:31 +0100

    fix

--- REE-DEB/usr/local/bin/node.sh
+++ REE-DEB/usr/local/bin/node.sh
@@ -2,6 +2,8 @@
 
 log "Starting node configuration"
 
+sudo cp /usr/local/templates/node/eth /etc/network/interfaces.d/eth
+
 sudo cp /usr/local/templates/node/start-batman-adv.sh /home/rpi/start-batman-adv.sh
 chmod +x /home/rpi/start-batman-adv.sh
 
