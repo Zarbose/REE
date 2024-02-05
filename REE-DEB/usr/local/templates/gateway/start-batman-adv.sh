0caba9f32fe6e83e07ea341b3fe6ab5ace1345d2
Zarbose <simonpieto.pieto@gmail.com>
2024-02-05 16:15:59 +0100

    Edit start batman replace eth0 to usb0

--- REE-DEB/usr/local/templates/gateway/start-batman-adv.sh
+++ REE-DEB/usr/local/templates/gateway/start-batman-adv.sh
@@ -8,9 +8,9 @@ sudo batctl gw_mode server
 
 # Enable port forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -i eth0 -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i bat0 -o eth0 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o usb0 -j MASQUERADE
+sudo iptables -A FORWARD -i usb0 -o bat0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i bat0 -o usb0 -j ACCEPT
 
 # Activates batman-adv interfaces
 sudo ifconfig wlan0 up
